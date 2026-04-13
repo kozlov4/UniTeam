255bda824a12f34f11737c49c6093b4ed66de1ea
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2026-04-13 19:59:02 +0300

    Додав шрифти та мінімально налаштував axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import { Route, Routes } from 'react-router'
-import './App.css'
+import { Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage/LoginPage.jsx";
+import RegisterPage from "./pages/LoginPage/LoginPage.jsx";
+import "./App.css";
 
 function App() {
-
   return (
     <Routes>
-      <Route path='/login' />
-      <Route path='/register'/>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/api/httpClient.js
+++ client/src/api/httpClient.js
@@ -1,1 +1,34 @@
 import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+const apiAxios = axios.create({
+  baseURL: BASE_URL,
+});
+
+apiAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+apiAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 401) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default apiAxios;
