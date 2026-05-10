fd004d4b9fcbbad0dac326b32928c4e9a60d3f74
Rostyslav <rostyslav.tarasov@nure.ua>
2026-05-10 11:58:07 +0300

    guest route fix

--- client/src/router/GuestRoute.jsx
+++ client/src/router/GuestRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const GuestRoute = () => {
   const isAuthenticated = !!localStorage.getItem("token");
 
-  return !isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
+  return !isAuthenticated ? <Outlet /> : <Navigate to="/dashboard" replace />;
 };
 
 export default GuestRoute;
